6f0d956d99d5d4b8d2d39459fffa3abad052de36
Ekidvu <ekidvu@gmail.com>
2023-09-01 23:46:54 +0300

    sections, main page

--- src/app/components/mp-text-block/text-block.tsx
+++ src/app/components/mp-text-block/text-block.tsx
@@ -2,9 +2,20 @@ import Link from "next/link";
 import s from "./styles.module.css"
 import Button from "../button/button";
 import cn from "classnames";
-// import TeslaPic from '../../../img/(main page) pic 1st Tesla Show.svg' 
-import teslaPic from '../../../img/(main page) pic 1st Tesla Show.png' 
-import TeslaIcon from '../../../img/Tesla icon.svg' 
+// import TeslaPic from '../../../img/(main page) pic 1st Tesla Show.svg'; 
+import teslaPic from '../../../img/(main page) pic 1st Tesla Show.png'; 
+import paperPic from '../../../img/(main page) pic 2 Paper Show.png'; 
+import cryoPic from '../../../img/(main page) pic 3 Cryo Show.png'; 
+import sciencePic from '../../../img/(main page) pic 4 Science Show.png'; 
+import soapBubblesPic from '../../../img/(main page) pic 5 Soap bubbles Show.png';
+import animationPic from '../../../img/(main page) pic 6 Animation.png'; 
+import reviewsPic from '../../../img/(main page) pic 7 Reviews.png'; 
+import TeslaIcon from '../../../img/Tesla icon.svg'; 
+import PaperIcon from '../../../img/Paper icon.svg'; 
+import CryoIcon from '../../../img/Cryo icon.svg'; 
+import ScienceIcon from '../../../img/Science icon.svg'; 
+import SoapBubblesIcon from '../../../img/Soap bubble icon.svg'; 
+import AnimationIcon from '../../../img/Animation icon.svg';
 
 export default function MainPageTextBlock(prop: {alignLeft: boolean, title: string, text: (string|React.ReactNode)[], icon: string, exceptions?: object | undefined}) {
     let titleSign: React.ReactNode;
@@ -13,7 +24,27 @@ export default function MainPageTextBlock(prop: {alignLeft: boolean, title: stri
     switch (prop.icon) {
         case "teslaIcon":
             titleSign = <TeslaIcon className={s.tb_icons} />;
-            blockPic = <img src={teslaPic.src} alt="" />;
+            blockPic = <img style={{translate: -180}} className={s.pic_move} src={teslaPic.src} alt="" />;
+            break;
+        case "paperIcon":
+            titleSign = <PaperIcon className={s.tb_icons} />;
+            blockPic = <img style={{translate: 60}} src={paperPic.src} alt="" />;
+            break;
+        case "cryoIcon":
+            titleSign = <CryoIcon className={s.tb_icons} />;
+            blockPic = <img style={{translate: -60}} src={cryoPic.src} alt="" />;
+            break;
+        case "scienceIcon":
+            titleSign = <ScienceIcon className={s.tb_icons} />;
+            blockPic = <img src={sciencePic.src} alt="" />;
+            break;
+        case "soapBubblesIcon":
+            titleSign = <SoapBubblesIcon className={s.tb_icons} />;
+            blockPic = <img style={{translate: -180}} className={s.pic_move} src={soapBubblesPic.src} alt="" />;
+            break;
+        case "animationIcon":
+            titleSign = <AnimationIcon className={s.tb_icons} />;
+            blockPic = <img style={{translate: 50}} src={animationPic.src} alt="" />;
             break;
     
         default:
@@ -22,11 +53,12 @@ export default function MainPageTextBlock(prop: {alignLeft: boolean, title: stri
 
 
     return (
-        <div className={s.container}>
+        <div className={cn(s.container)}>
             <div className={s.cont_img}>
                 {blockPic}
             </div>
             <div className={cn(s.cont_text, {
+                [s.container_left]: prop.alignLeft,
                 [s.container_right]: !prop.alignLeft,
             })}>
                 <h1 className={s.tb_title}>
