e4f349d1623c6cf99ebdc858778361dffd5b4ec5
Ekidvu <ekidvu@gmail.com>
2023-09-01 19:33:57 +0300

    new section

--- src/app/components/mp-text-block/text-block.tsx
+++ src/app/components/mp-text-block/text-block.tsx
@@ -2,20 +2,34 @@ import Link from "next/link";
 import s from "./styles.module.css"
 import Button from "../button/button";
 import cn from "classnames";
-import TeslaIcon from '../../../img/(main page) pic 1st Tesla Show.svg'
-import TesIc from '../../../img/планета пнг 1.png' 
+import TeslaPic from '../../../img/(main page) pic 1st Tesla Show.svg' 
+// import teslaPic from '../../../img/(main page) pic 1st Tesla Show.png' 
+import TeslaIcon from '../../../img/Tesla icon.svg' 
 
 export default function MainPageTextBlock(prop: {alignLeft: boolean, title: string, text: (string|React.ReactNode)[], icon: string, exceptions?: object | undefined}) {
-  
+    let titleSign: React.ReactNode;
+
+    switch (prop.icon) {
+        case "teslaIcon":
+            titleSign = <TeslaIcon className={s.tb_icons} />;
+            break;
+    
+        default:
+            break;
+    }
+
+    // const teslaPic: StaticImageData = teslaPic;
     // const icon = `${prop.icon}`;
-    console.log(prop.text);
+    console.log(titleSign);
     // const img: Element = <TeslaIcon/> 
 
     return (
         <div className={s.container}>
             <div className={s.cont_img}>
-                <TeslaIcon />
-                {/* <img src="../../../img/планета пнг 1.png" 
+                {/* {teslaPic} */}
+                {/* <teslaPic /> */}
+                <TeslaPic />
+                {/* <img src="../../../img/(main page) pic 1st Tesla Show.png" 
                 width={300}
                 height={300}
                 alt="" /> */}
@@ -23,13 +37,15 @@ export default function MainPageTextBlock(prop: {alignLeft: boolean, title: stri
             <div className={cn(s.cont_text, {
                 [s.container_right]: !prop.alignLeft,
             })}>
-                <h1 className={s.tb_title}>{prop.title}</h1>
+                <h1 className={s.tb_title}>{titleSign}{prop.title}</h1>
                 <div className={s.tb_text}>
                     { prop.text.map((el,i) => <div key={i}>{el}</div>) }
-                    {/* // {prop.text.map((el,i) => { 
-                    //     if(!!prop.exceptions.length && el.includes())
-                    // <div key={i}>{el}</div>
-                    // })} */}
+                    {/* {prop.text.map((el,i) => {
+                        console.log(typeof el);
+                         
+                        if(el?.key) return el
+                        else return <div key={i}>{el}</div>
+                    })} */}
                 </div>
                 <div className={s.tb_button}>
                     <Button round={false} text="ПОДРОБНЕЕ" />
@@ -39,3 +55,6 @@ export default function MainPageTextBlock(prop: {alignLeft: boolean, title: stri
 
     )
 }
+
+
+// if(el?.type === "span") return el
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,8 @@ import s from './page.module.css';
 import cn from 'classnames';
 import Button from './components/button/button';
 import LogoIcon from '../img/logo_1.svg';
-import bg_image from '../img/bg_main_page_image.svg'
+import TeslaPlusIcon from '../img/tesla_plus_icon.svg'
+import TeslaMinusIcon from '../img/tesla_minus_icon.svg'
 import MainPageTextBlock from "./components/mp-text-block/text-block";
 
 export default function Home() {
@@ -35,10 +36,10 @@ export default function Home() {
           alignLeft={false} 
           title="ТЕСЛА ШОУ" 
           text={["У вас есть уникальная возможность зарядить себя и своих друзей позитивной энергией настоящих молний.", 
-          <span key={1}>Примечание:</span>, "В результате проведения Тесла шоу у людей открываются SUPER способности!", 
-          "Меняем жизненный заряд с       на        уже 10 лет.", 
+          <div key={1}><span key={2} className="tb_bold">Примечание:</span>&nbsp;В результате проведения Тесла шоу у людей открываются <span key={3} className="tb_yellow">SUPER</span>&nbsp;способности!</div>,
+          <div key={4} className="div_icons_in_text">Меняем жизненный заряд с&nbsp;<span><TeslaMinusIcon/>&nbsp;</span>на<span>&nbsp;<TeslaPlusIcon/>&nbsp;</span>уже 10 лет.</div>, 
           "Программа для тех, кто хочет зарядиться чистой энергией и обрести супер способности."]}
-          icon={'../img/(main page) pic 1st Tesla Show.svg'}
+          icon='teslaIcon'
           exceptions={{"Примечание:": "s.tb_bold", "SUPER": "s.tb_yellow"}}
         />
       </section>
