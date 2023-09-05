76857b9b5c92a67c20a2812e5644557d30b3a73a
Ekidvu <ekidvu@gmail.com>
2023-09-05 16:50:48 +0300

    fonts

--- src/app/pages/_document.js
+++ src/app/pages/_document.js
@@ -14,6 +14,8 @@ class MyDocument extends Document {
                     <link href="https://fonts.googleapis.com/css2?family=Fira+Mono:wght@500;700&display=swap" rel="stylesheet" />
                     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Display:ital,wght@0,500;0,700;1,500;1,700&display=swap" rel="stylesheet"></link>
                     <link href="https://fonts.googleapis.com/css2?family=Amiko:wght@400;600;700&display=swap" rel="stylesheet"></link>
+
+                    <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet"></link>
                 </Head>
                 <body>
                     <Main />
