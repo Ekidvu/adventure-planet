ff67777c2ddb584f44bd3c9e34471de124bbf9b2
Ekidvu <ekidvu@gmail.com>
2023-10-25 15:37:38 +0300

    bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,39 +2,53 @@
 
 import './globals.css';
 import './variables.css';
-import type { Metadata } from 'next';
+import type { Metadata, ResolvingMetadata } from 'next';
 import { Inter } from 'next/font/google';
 import { metadataInfo } from './show-list-database';
 import Navbar from './components/navbar/navbar';
 import { usePathname } from 'next/navigation';
 
 const inter = Inter({ subsets: ['latin'] })
-const pathname = usePathname();
 let descriptionSugar: string = "Тесла шоу, мыльное шоу бумажное шоу, крио шоу, научное шоу, шоу мыльных пузырей, анимация.";
 
-export const metadata: Metadata = {
-  title: `${metaInfo(pathname)?.titleName} | Планета Приключений`,
-  description: `Шоу Планета Приключений - ${metaInfo(pathname)?.description}. ${descriptionSugar}`,
-  openGraph: {
-    title: `${metaInfo(pathname)?.titleName} | Планета Приключений`,
-    description: `Шоу Планета Приключений - ${metaInfo(pathname)?.description}. ${descriptionSugar}`,
-    url: '',
-    siteName: 'Планета Приключений',
-    images: [
-      {
-        url: '/public/logo_meta.jpg',
-      },
-    ],
-    locale: 'ru_RU',
-    type: 'website',
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: `${metaInfo(pathname)?.titleName} | Планета Приключений`,
-    description: `Шоу Планета Приключений - ${metaInfo(pathname)?.description}. ${descriptionSugar}`,
-    images: ['/public/logo_meta.jpg'],
+type Props = {
+  params: { id: string }
+  searchParams: { [key: string]: string | string[] | undefined }
+}
+ 
+export async function generateMetadata(
+  // { params, searchParams }: Props,
+  // parent: ResolvingMetadata
+): Promise<Metadata> {
+  const pathname = usePathname();
+  const metaInfo = metadataInfo.find(el => el.page === pathname)
+  
+  
+  return {
+    title: `${metaInfo?.titleName} | Планета Приключений`,
+    description: `Шоу Планета Приключений - ${metaInfo?.description}. ${descriptionSugar}`,
+    openGraph: {
+      title: `${metaInfo?.titleName} | Планета Приключений`,
+      description: `Шоу Планета Приключений - ${metaInfo?.description}. ${descriptionSugar}`,
+      url: '',
+      siteName: 'Планета Приключений',
+      images: [
+        {
+          url: '/public/logo_meta.jpg',
+        },
+      ],
+      locale: 'ru_RU',
+      type: 'website',
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `${metaInfo?.titleName} | Планета Приключений`,
+      description: `Шоу Планета Приключений - ${metaInfo?.description}. ${descriptionSugar}`,
+      images: ['/public/logo_meta.jpg'],
+    }
   }
 }
+
 
 export default function RootLayout({
   children,
@@ -53,6 +67,6 @@ export default function RootLayout({
 }
 
 
-function metaInfo(path: string) {
-  return metadataInfo.find(el => el.page === path);
-}
+// function metaInfo(path: string) {
+//   return metadataInfo.find(el => el.page === path);
+// }
